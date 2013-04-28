1c3d70d2946017c43c9c7a75a48ed104dee41891
srikanth <srikanth@srikanth-Lenovo-Z580>
2013-04-28 01:57:42 -0500

    Checking inserver handlers.

--- Server/model.py
+++ Server/model.py
@@ -14,6 +14,7 @@ import logging
 import os
 
 from google.appengine.ext import ndb
+from google.appengine.datastore.datastore_query import Cursor
 
 class User(ndb.Model):
     """Models an individual Quora+ User"""
@@ -37,147 +38,212 @@ class User(ndb.Model):
             return user
         return None
 
+    @classmethod
+    def is_valid_user(cls, self, email, password):
+        key     = ndb.Key(User, email)
+        user = key.get()
+        if user is None:
+            return False
+        elif password == user.password:
+            return True
+        return False
+    
+    @classmethod
+    def get_user(cls, email):
+        key = ndb.Key(User, email)
+        user = key.get()
+        return user
 
 class Question(ndb.Model):
     """Models an individual Question on  Quora+"""
     date_created = ndb.DateProperty(indexed=True, auto_now=True)
     description = ndb.TextProperty(indexed=True)
+    circles = ndb.StringProperty(repeated=True)
+    email = ndb.StringProperty(indexed=True)
 
     @classmethod 
     @ndb.transactional(retries=1)
-    def create_question(cls, email, description):
-        parent = ndb.Key(User, email)
-        qry = Question.query(ndb.AND(Question.parent == parent,
+    def create_question(cls, email, description, circles):
+        qry = Question.query(ndb.AND(Question.email == email,
                                      Question.description == description))
 
         if len(qry) == 0:
             question = Question(email=email,
-                                password=password,
-                                first_name=first_name,
-                                last_name=last_name)
-            question.key().parent = parent
+                                description=description,
+                                circles=circles)
             question.put()
             return True
         return False
 
+    @classmethod
+    def fetch_questions(cls, email, curs, circle, question_ids):
+        allowed_emails = []
+        if circle is None:
+            qry = Contact.query(ndb.AND(Contact.user_email == email, circle in Contact.circles))
+        else:
+            qry = Contact.query(Contact.email == email)
+        for row in qry:
+            allowed_emails.append(row.email)
+
+        qry = Contact.query(ndb.AND(contact.emal == email, contact.user_email in allowed_emails))
+        circles = []
+        for row in qry:
+            circles.append(row.circles)
+        if question_ids is None:
+            qry, next_curs, more = Question.query(ndb.AND(len(set(circles).intersection(set(Question.circles))) > 0,
+                                                          Question.email in allowed_emails)).fetch_page(20, start_cursor = curs)
+        else:
+            qry, next_curs, more = Question.query(Question.key().integer_id() in question_ids).fetch_page(20, start_cursor = curs)
+        return next_curs, more, qry.map(Answer.callback)
+        
+
 class Notification(ndb.Model):
     """Models user notification on  Quora+"""
-    date_created = ndb.DateProperty(indexed=True)
-    description = ndb.StringProperty(indexed=True)
+    date_created = ndb.DateProperty(indexed=True, auto_now_add=True)
+    data = ndb.StringProperty(indexed=False, repeated=True)
     is_read = ndb.BooleanProperty(default=False, indexed=True)
+    email = ndb.StringProperty(indexed=True)
+    creator_email = ndb.StringProperty(indexed=False)
+    type = ndb.IntegerProperty(indexed=True)  # 1 for addition, 2 for answering 
+
     @classmethod 
     @ndb.transactional(retries=1)
-    def create_notification(cls, email, description):
-        parent = ndb.Key(User, email)
-        notification = Notification(description=description)
+    def create_notification(cls, email, data, creator_email, type):
+        notification = Notification(data=data, email=email,
+                                    creator_email=creator_email, type=type)
         notification.put()
         return True
 
     @classmethod 
     @ndb.transactional(retries=1)
-    def mark_as_read(cls, email, identifier):
-        key = ndb.Key(User, email, Notification, identifier)
-        notification = key.get()
-        notification.is_read = True
-        notification.put()
-        return True
+    def mark_as_read(cls, email, identifier=None):
+        if identifier is None:
+            notifications = Notification.query(ndb.AND(email==email, is_read==False))
+        else:
+            key = ndb.Key(Notification, identifier)
+            notifications = [key.get()]
+        for notification in notifications:
+            notification.is_read = True
+            notification.put()
+        return "Success"
+
+    @classmethod
+    def fetch_no_of_unread_notifications(cls, email):
+         return len(Notification.query(ndb.AND(email==email, is_read==False)))
+    
+    @classmethod
+    def fetch_unread_notifications(cls, email):
+        notifications, next_cur, more = Notifications.query(ndb.AND(email==email, is_read==False)).fetch_page(20, start_cursor=curs)
+        return notifications, next_cur, more
 
 class Circle(ndb.Model):
-    """Models circle membership on  Quora+"""
+    """Models circles on  Quora+"""
     date_created = ndb.DateProperty(indexed=False, auto_now_add=True)
     description = ndb.StringProperty(indexed=False)
     name = ndb.StringProperty(indexed=True)
+    email = ndb.StringProperty(indexed=True)
 
     @classmethod 
     @ndb.transactional(retries=1)
-    def create_circle(cls, email, name, description):
-        parent = ndb.Key(User, email)
-        key = ndb.Key(Circle, name, parent=parent)
-        if key.get() is None:
-            circle = Circle(key=key,
-                            description=description,
+    def create_circle(cls, name, email, description):
+        qry = Circle.query(ndb.AND(Circle.name == name, email == email))
+        if len(qry) == 0:
+            circle = Circle(description=description,
+                            email = email,
                             name=name)
             circle.put()
             return True
         return False
+
+    @classmethod
+    def fetch_circles(cls, email):
+        qry = Circle.query(Circle.email == email).fetch(projection=[Circle.name])
+        return qry
 
 class Answer(Question):
     """Models answer on Quora+"""
     description = ndb.StringProperty(indexed=False)
     date_created = ndb.DateProperty(auto_now_add=True)
     email = ndb.StringProperty(indexed=True)
+    upvote_count = ndb.IntegerProperty(default=0, indexed=True)
+
+    def callback(self, question):
+        answer = Answer.query(parent=question.key()).fetch().order(-Answer.upvote_count)
+        answer_id = answer.key().integer_id()
+        voters = Vote.fetch_voters(answer_id)
+        return question, answer[0], len(answer), voters
 
     @classmethod
     @ndb.transactional(retries=1)
-    def create_answer(cls, question_id, author_email, description, email):
-        parent = ndb.Key(Question, question_id, User, email)
+    def create_answer(cls, question_id, email, description):
+        parent = ndb.Key(Question, question_id)
         answer = Answer(description=description,
-                        email=author_email
-                        )
+                        email=email)
         answer.put()
         return True
 
 class Vote(ndb.Model):
     """Models upvoting on Quora+"""
     email = ndb.StringProperty(indexed=True)
-    state = ndb.IntegerProperty(indexed=True, default=1)
+    state = ndb.IntegerProperty(indexed=True, default=1) #1 upvote, 0 downvote
+    answer_id = ndb.IntegerProperty(indexed=True)
+    name = ndb.StringProperty(indexed=False)
     @classmethod
     @ndb.transactional(retries=1)
-    def create_or_update_vote(cls, answer_id, voter_email, email, question_id):
-        parent = ndb.Key(User, email, Question, question_id, Answer, answer_id)
-        key = ndb.Key(Vote, voter_email, parent=parent)
-        if key.get() is None:
-            vote = Vote(key=key, email=voter_email)
+    def create_or_update_vote(cls, answer_id, email, name, state):
+        qry = Vote.query(ndb.AND(Vote.email == email,
+                                 Vote.answer_id == answer_id))
+        if len(qry) == 0:
+            vote = Vote(email=voter_email, answerd_id=answer_id, name=name, state=state)
             vote.put()
         else:
-            vote = key.get()
+            vote = qry[0]
             vote.state = 1 - vote.state
             vote.put()
         return True
 
+    @classmethod
+    def fetch_voters(cls, answer_id):
+        return Vote.query(ndb.Query(Vote.answer_id==answer_id, Vote.state==1)).fetch(projection=[Vote.name, Vote.email])
+
 class Favorite(ndb.Model):
     """Models favorite on Quora+"""
-    question_id = ndb.StringProperty(indexed=True)
+    question_ids = ndb.StringProperty(indexed=True, repeated=True)
+    email = ndb.StringProperty(indexed=True)
     @classmethod
     @ndb.transactional(retries=1)
     def create_or_update_favorite(cls, email, question_id):
-        parent = ndb.Key(User, email)
-        key = ndb.Key(Favorite, question_id, parent=parent)
+        key = ndb.Key(User, email)
         if key.get() is None:
-            favorite = Favorite(question_id=question_id)
+            favorite = Favorite(question_ids=[question_id])
             favorite.put()
         else:
-            favorite = qry[0]
-            favoite.key.delete()
+            favorite = key.get()
+            if question_id in favorite.question_ids:
+                favorite.question_ids = favorite.question_ids.remove(question_id)
+            else:
+                favorite.questions_ids.append(question_id)
+            favorite.put()
         return True
 
-class Share():
-    """Models sharing to circles on Quora+"""
-    circle_name = ndb.StringProperty(indexed=True)
-    date_created = ndb.DateTimeProperty(indexed=False, auto_now=True)
     @classmethod
-    @ndb.transactional(retries=1)
-    def create_share(cls, circle_name, email, question_id):
-        parent = ndb.Key(User, email, Question, question_id)
-        key = ndb.Key(Share, circle_name, parent=parent)
-        if key.get() is None:
-            share = Share(key=key, circle_name=circle_name)
-            share.put()
-            return True
-        return False
+    def is_favorite(cls, email, question_id):
+        favorites = Favorite.query(Favorite.email==email).question_ids
+        return question_id in favorites
 
 class Contact(User):
     """Models individual contacts on Quora+"""
     email = ndb.StringProperty(indexed=True)
     date_created = ndb.DateTimeProperty(indexed=True, auto_now=True)
-    circle_names = ndb.StringProperty(repeated=True, indexed=False)
+    circles = ndb.StringProperty(repeated=True, indexed=False)
+    user_email = ndb.StringProperty(indexed=True)
     @classmethod
     @ndb.transactional(retries=1)
-    def create_contact(cls, circle_names, contact_email, email):
-        parent = ndb.Key(User, email)
-        key = ndb.Key(Contact, contact_email, parent=parent)
-        if key.get() is None:
-            contact = Contact(key=key, email=contact_email, cicle_names=circle_names)
+    def create_contact(cls, circles, email, user_email, name):
+        qry = Contact.query(ndb.AND(Contact.email == email,
+                                    Contact.user_email == user_email))
+        if len(qry) == 0:
+            contact = Contact(email=email, user_email=user_email, name=name, cicles=circles)
             contact.put()
             return True
         return False
--- Server/web.py
+++ Server/web.py
@@ -50,31 +50,31 @@ class CreateUser(webapp2.RequestHandler):
 
 class CreateQuestion(webapp2.RequestHandler):
     def post(self, user_id=None):
-        question = Question.create_question(
+        circles = simplejson.loads(self.request.get("circles"))
+        result = Question.create_question(
                     email=user_id,
-                    description=self.request.get("description")
+                    description=self.request.get("description"),
+                    circles=circles
                    )
-        question_id = question.key().id()
-        circles_list = simplejson.loads(self.request.get("circles_list"))
-        for circle_name in circles_list:
-            share = create_share(
-                    circle_name=circle_name,
-                    email=email,
-                    question_id=question_id
-                    )
-            if share is None:
-                self.response.out.write("Failure")
-        else:
+        
+        if result:
             self.response.out.write("Success")
+        else:
+            self.response.out.write("Failure")
 
 class CreateAnswer(webapp2.RequestHandler):
     def post(self, question_id=None):
         answer = Answer.create_answer(
-                        question_id=question_id,
-                        author_email=self.request.get("author_email"),
+                        question_id=int(question_id),
+                        email=self.request.get("email"),
                         description=self.request.get("description"),
-                        email=self.request.get("email")
                     )
+        key = ndb.Key(Question, int(question_id))
+        email = key.get().email
+        Notification.create_notification(email=email,
+                                         data=[self.request.get("email"), self.request.get("name")], 
+                                         creator_email=self.request.get("email"),
+                                         type=2)
         if answer is None:
             self.response.out.write("Failure")
         else:
@@ -94,42 +94,113 @@ class CreateCircle(webapp2.RequestHandler):
 
 class CreateContact(webapp2.RequestHandler):
     def post(self, user_id=None):
-        cicle_names = simplejson.loads(self.request.get("circle_names"))
+        cicle_names = simplejson.loads(self.request.get("circles"))
         contact = Contact.create_contact(
                         circle_names=circle_names,
                         contact_email=self.request.get("email"),
-                        email=user_id
+                        email=user_id,
+                        name=self.request.get("name")
                     )
+        qry = Contacts.query(ndb.AND(email=user_id, user_email=self.request.get("email"))).fetch()
+        if len(qry) > 0:
+            data = qry[0].circles
+        Notification.create_notification(email=contact_email, creator_email=email, type=1, data=circles)
         if contact is None:
             self.response.out.write("Failure")
         else:
             self.response.out.write("Success")
             
-class Vote(webapp2.RequestHandler):
+class MarkVote(webapp2.RequestHandler):
     def post(self, answer_id=None):
         vote = Vote.create_or_update_vote(
                         answer_id=answer_id,
-                        voter_email=self.request.get("voter_email"),
                         email=self.request.get("email"),
-                        question_id=question_id
+                        name=self.request.get("name"),
+                        state=self.request.get("state")
                     )
         if vote is None:
             self.response.out.write("Failure")
         else:
             self.response.out.write("Success")
 
-class Favorite(webapp2.RequestHandler):
-    def post(self, answer_id=None):
-        favorite = favorite.create_or_update_favorite(
-                        answer_id=answer_id,
-                        voter_email=self.request.get("voter_email"),
-                        email=self.request.get("email"),
+class MarkFavorite(webapp2.RequestHandler):
+    def post(self, user_id=None):
+        favorite = Favorite.create_or_update_favorite(
+                        email=user_id,
                         question_id=self.request.get("question_id")
                     )
         if favorite is None:
             self.response.out.write("Failure")
         else:
             self.response.out.write("Success")
+
+def LoadHeader(email):
+    result = {}
+    result['no_of_notifications'] = fetch_no_of_unread_notifications(email)
+    result['circles'] = fetch_circles(email) #projected only circle name
+    return simplejson.dumps(result);
+    
+class Login(webapp2.RequestHandler):
+    def post(self):
+        if User.is_valid_user(self.request.get("email"),
+                              self.request.get("password")):
+            return LoadHeader(self.request.get("email"))
+        else:
+            self.response.out.write("Failure")
+
+class MainPage(webapp2.RequestHandler):
+    def post(self):
+        results = {}
+        email = self.request.get("email")
+        curs = Cursor(urlsafe=self.request.get("cursor"))
+        if self.request.get("circle_name"):
+            circle = self.request.get("circle_name")
+        else:
+            circle = None
+        question_list = []
+        if self.request.get("want_favorites"):
+            qry = Favorite.query(email==email).fetch()
+            for row in qry:
+                question_list.append(row.key().integer_id())
+        if self.request.get("question_id"):
+            question_list.append(int(self.request.get("question_id")))
+        next_curs, more, questions = fetch_question(email, curs, circle, question_list)
+        result["next_curs"] = next_curs
+        result["more"] = more
+        list = []
+        for tuple in questions:
+            #question, top answer, # of answers, voters
+            is_upvote = False
+            voters = []
+            for voter in tuple[3]:
+                voters.append(voter.name)
+                if email == voter.email:
+                    is_upvote = True
+            is_favorite = Favorite.is_favorite(email, tuple[0].key.integer_id())
+            list.append([tuple[0], tuple[1], tuple[2], tuple[3], voters, is_upvote, is_favorite])
+        result["questions"] = list
+        return simplejson.dumps(result)
+
+class NotificationsPage(webapp2.RequestHandler):
+    def post(self):
+        email = self.request.get("email")
+        curs = Cursor(urlsafe=self.request.get("cursor"))
+        notifications, next_curs, more = Notification.fetch_unread_notifications(email)
+        result = {}
+        result["notifications"] = notifications
+        result["next_curs"] = next_curs.url_safe()
+        result["more"] = more
+        return simplejson.dumps(result)
+
+class ClearNotificationsPage(webapp2.RequestHandler):
+    def post(self):
+        email = self.request.get("email")
+        return Notification.mark_as_read(email=email)
+
+class FavoritePage(webapp2.RequestHandler):
+    def post(self):
+        email = self.request.get("email")
+        
 ###### ROUTES and WSGI STUFF ######
 url_routes = []
 # url_routes.append(
@@ -176,17 +247,39 @@ url_routes.append(
  
 url_routes.append(
     routes.RedirectRoute(r'/<answer_id:\d+>/vote',
-                         handler=Vote,
+                         handler=Mark_Vote,
                          strict_slash=True,
                          name="vote")
 )
  
 url_routes.append(
-    routes.RedirectRoute(r'/<user_id:\d+>/favorite',
-                         handler=Favorite,
+    routes.RedirectRoute(r'/<user_id:\d+>/create_favorite',
+                         handler=Mark_Favorite,
                          strict_slash=True,
                          name="favorite")
 )
+
+url_routes.append(
+    routes.RedirectRoute(r'/login',
+                         handler=Login,
+                         strict_slash=True,
+                         name="login")
+)
+
+url_routes.append(
+    routes.RedirectRoute(r'/main',
+                         handler=MainPage,
+                         strict_slash=True,
+                         name="main")
+)
+
+url_routes.append(
+    routes.RedirectRoute(r'/notifications',
+                         handler=NotificationsPage,
+                         strict_slash=True,
+                         name="notifications")
+)
+
 # 
 # url_routes.append(
 #     routes.RedirectRoute(r'/<user_id:\d+>/view_contacts',
@@ -195,11 +288,4 @@ url_routes.append(
 #                          name="view_contacts")
 # )
 #  
-# url_routes.append(
-#     routes.RedirectRoute(r'/<question_id:\d+>',
-#                          handler=ViewQuestion,
-#                          strict_slash=True,
-#                          name="view_question")
-# )
-
 app = webapp2.WSGIApplication(url_routes)
